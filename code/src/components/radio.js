d8b8a0b03fb229512b951d72482e91e736559dc9
Cecilia Wong <cissi.wong@gmail.com>
2017-12-05 14:24:43 +0100

    added background color

--- code/src/components/radio.js
+++ code/src/components/radio.js
@@ -4,10 +4,9 @@ class Radio extends React.Component {
 
   render() {
     return (
-      <div>
+      <div style={{ backgroundColor: `#${this.props.color}` }}>
         <div>{this.props.name}</div>
         <img src={this.props.image} alt="" />
-        <div>{this.props.color}</div>
         <audio controls src={this.props.audio} type="audio/mpeg" ><track kind="captions" /></audio>
       </div>
     )
